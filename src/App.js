38dd20d7db5e7c48e484477d1a13a3011b90ef0d
agusnrossi <71663682+agusnrossi@users.noreply.github.com>
2021-07-28 22:44:53 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo rufit fondo negro.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenido a la tienda online de Rufit.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
