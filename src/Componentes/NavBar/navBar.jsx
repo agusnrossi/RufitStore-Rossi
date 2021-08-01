4568a5a967b1e1b2e32d6c42f973af7bf0ce6f44
Agustin Rossi <agusrossi021287@gmail.com>
2021-07-31 22:08:51 -0300

    cambio de categorias

--- src/Componentes/NavBar/navBar.jsx
+++ src/Componentes/NavBar/navBar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import logoRufit from '../../assets/img/logoRufit.png'
-import { Navbar,Nav,NavDropdown, Container } from "react-bootstrap";
+import logoRufit from "../../assets/img/logoRufit.png";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 
 export const NavBar = () => {
   return (
     <div>
       <>
- 
-    
-      
         <br />
-        <Navbar fixed="top" collapseOnSelect expand="lg"  bg="dark" variant="dark">
+        <Navbar
+          fixed="top"
+          collapseOnSelect
+          expand="lg"
+          bg="dark"
+          variant="dark"
+        >
           <Container>
             <Navbar.Brand href="#home">
               <img
@@ -19,27 +22,30 @@ export const NavBar = () => {
                 width="130"
                 height="100"
                 className="d-inline-block align-top"
-              />{" "}            
+              />{" "}
             </Navbar.Brand>
             <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
-      <Nav.Link href="#action1">Home</Nav.Link>
-      <Nav.Link href="#action2">Link</Nav.Link>
-      <NavDropdown title="Link" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-      </NavDropdown>
-      <Nav.Link href="#" disabled>
-        Link
-      </Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
+              <Nav
+                className="mr-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <NavDropdown title="Productos" id="navbarScrollingDropdown">
+                  <NavDropdown.Item href="#action3">Remeras</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Suplementos
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Accesorios
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                  Contacto
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
           </Container>
         </Navbar>
       </>
