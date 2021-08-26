22d7de584cb73becfd459c82591351fa2daca279
Agustin Rossi <agusrossi021287@gmail.com>
2021-08-25 23:08:30 -0300

    arreglo desafio 10

--- src/Componentes/NavBar/CartWidget.js
+++ src/Componentes/NavBar/CartWidget.js
@@ -1,15 +1,19 @@
+import React, { useContext } from "react";
+import { MdShoppingCart } from "react-icons/md";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { CartContext } from "../Context/CartContext";
 
-import React from 'react';
-import {MdShoppingCart} from 'react-icons/md';
-import{Button} from 'react-bootstrap';
-
-class CartWidget extends React.Component{
-    render(){
-
-        return   <Button variant="danger"><MdShoppingCart/></Button> 
-
-
-    }
-
+function CartWidget() {
+  const { cartSize } = useContext(CartContext);
+  return (
+    <Link to="/cart">
+      <Button variant="danger">
+        <MdShoppingCart />
+        <span style={{ marginRight: 250 }}>{cartSize}</span>
+      </Button>
+    </Link>
+  );
 }
+
 export default CartWidget;
