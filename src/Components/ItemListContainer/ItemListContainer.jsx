aa27936863b37405dd46028086f429d194bfff1c
Agustin Rossi <agusrossi021287@gmail.com>
2021-09-21 07:37:09 -0300

    cambios en readme

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,14 +1,9 @@
 import { React, useState, useEffect } from "react";
 //import { productos } from "../../Helpers/Productos";
 import ItemList from "../Item/ItemList";
-import { getFirestore } from "../firebase/firebase";
 
 function ItemListContainer(props) {
   const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    getFirestore();
-  }, []);
 
   return (
     <div>
